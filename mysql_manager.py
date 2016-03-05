55ce5e07abc23d5e18cc6e831d86ce3d7b112e2a
Jeet Nagda <jeetnagda@gmail.com>
2016-03-05 11:04:28 -0800

    bug_fixing mysql_manager

--- mysql_manager.py
+++ mysql_manager.py
@@ -1,5 +1,15 @@
+"""
+Created by Jeet Nagda
+To Use: please import right before you call it, and close immediately after finish
+import mysql_manager as mm
+result = mm.get_list....
+mm.close_connection()
+
+"""
+
 import mysql.connector
 
+# Configuration for mysql database
 config = {
     'user': 'ucifooddy',
     'password': 'ucifooddy',
@@ -12,6 +22,10 @@ cnx = mysql.connector.connect(**config)
 
 
 def get_list_of_category_names():
+    """
+    Function to return list of category names as list(tuples(string,))
+    :return: category names as list(tuples(string,))
+    """
     curr = cnx.cursor()
     curr.execute('SELECT category_name From Categories')
     result = curr.fetchall()
@@ -20,6 +34,10 @@ def get_list_of_category_names():
 
 
 def get_list_of_category_alias():
+    """
+    Function to return list of category aliases names as list(tuples(string,))
+    :return: category alaises list(tuples(string,))
+    """
     curr = cnx.cursor()
     curr.execute('SELECT category_alias FROM Categories')
     result = curr.fetchall()
@@ -28,37 +46,58 @@ def get_list_of_category_alias():
 
 
 def get_list_of_category_names_user_likes(username):
+    """
+    Function to get list of category names that the user likes
+    :param username of profile
+    :return: list(tuples(string,))
+    """
     curr = cnx.cursor()
-    curr.execute('SELECT category_name FROM Categories,UserProfile,UserWeights '
-                 'WHERE C'
-                 'ategories.category_id = UserWeights.category_id '
-                 'AND UserWeights.weight > 0 '
-                 'AND UserProfile.username = %s',(username))
+    curr.execute('SELECT category_name FROM UserProfile,UserWeights,Categories '
+                 'WHERE UserWeights.category_id = Categories.category_id AND '
+                 'UserProfile.user_name = UserWeights.user_name '
+                 'AND UserWeights.weight > 0.0'
+                 'AND UserProfile.user_name = %s',(username,))
     result = curr.fetchall()
     curr.close()
     return result
 
 
 def get_category_weights_for_user(username):
+    """
+    Function to get list of category names and weights, note need to cast tuple Decimal as float
+    :param username of profile
+    :return: list(tuples(string,Decimal))
+    """
     curr = cnx.cursor()
-    curr.execute('SELECT category_name,weight FROM Categories,UserWeights,UserProfile '
-                 'WHERE Categories.category_id = UserWeights.category_id AND'
+    curr.execute('SELECT category_name,weight FROM UserProfile,UserWeights,Categories '
+                 'WHERE UserWeights.category_id = Categories.category_id AND '
                  'UserProfile.user_name = UserWeights.user_name '
-                 'AND UserWeight.weight > 0.0'
-                 'AND UserProfile.username = %s',(username))
+                 'AND UserWeights.weight > 0.0'
+                 'AND UserProfile.user_name = %s',(username,))
     result = curr.fetchall()
     curr.close()
     return result
 
 
 def get_number_of_categories():
+    """
+    Function to get number of categories table
+    :return: int
+    """
     curr = cnx.cursor()
-    curr.execute('SELECT COUNT(*) FROM Categorires')
+    curr.execute('SELECT COUNT(*) FROM Categories')
     result = curr.fetchone()
     curr.close()
-    return result
+    return result[0]
+
 
 def is_login_valid(username,password):
+    """
+    Function to check if given login information is valid
+    :param username: user_names as profile, used as PK
+    :param password: password to check if valid
+    :return: Boolean response if login is accepted or not
+    """
     curr = cnx.cursor()
     curr.execute('SELECT password FROM UserProfile WHERE user_name = %s',(username,))
     password_tmp = curr.fetchone()
@@ -69,15 +108,27 @@ def is_login_valid(username,password):
 
 
 def insert_category(category_name,category_alias):
+    """
+    Function to insert new category record
+    :param category_name: formal name of category to insert Ex: "Italian"
+    :param category_alias: alias used by Yelp API to search Ex: "italian"
+    :return: None
+    """
     curr = cnx.cursor()
-    curr.execute('INSERT INTO Categories(category_name,category_alias)',(category_name,category_alias))
+    curr.execute('INSERT IGNORE INTO Categories(category_name,category_alias)',(category_name,category_alias))
     cnx.commit()
     curr.close()
 
 
 def insert_new_user_profile(username,password):
+    """
+    Function to insrt new user_profile into database
+    :param username: PK user_name to insert
+    :param password: Password of new user
+    :return: None
+    """
     curr = cnx.cursor()
-    curr.execute('INSERT INTO UserProfile(user_name,password) VALUES (%s,%s)',(username,password))
+    curr.execute('INSERT IGNORE INTO UserProfile(user_name,password) VALUES (%s,%s)',(username,password))
     cnx.commit()
     curr.close()
 
