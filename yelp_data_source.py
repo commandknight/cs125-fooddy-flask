3ecbde40e0c631d37ec043ad40106156a1ab1d56
tfeng <tbhfeng@gmail.com>
2016-03-09 00:34:57 -0800

    Added get_bounding_box that takes in a list of two coordinates tuples and returns 4 points that acts as bounding box.

--- yelp_data_source.py
+++ yelp_data_source.py
@@ -6,7 +6,8 @@ import urllib;
 import json;
 import mysql_manager as mm
 import numpy as np;
-
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
 
 class YelpData:
 
@@ -34,9 +35,18 @@ consumer_secret = 'JTZY_0nE8ohfazCK-e_hP_aHhDs'
 token = 'Sa67MrlQ3hoVCr3Wsn2rfU9-kPPQ40Q_'
 token_secret = '8Xyb1WAJpRW_TJFomJb3eom3e4w'
 
-google_places_key="AIzaSyBrElDm-bOxHup93M1QLfWXYjpYYoReGjg"
+google_places_key = "AIzaSyBrElDm-bOxHup93M1QLfWXYjpYYoReGjg"
 
 yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
+
+auth = Oauth1Authenticator(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    token=token,
+    token_secret=token_secret
+)
+
+yelp_client = Client(auth)
 
 category_dict = mm.get_category_dict();
 num_categories = len(category_dict)
@@ -46,7 +56,7 @@ category_name_to_alias_dict = mm.get_category_name_to_alias_dict();
 # Query the Yelp API for a Business, by passing restaurant_id.
 def get_business_by_id(restaurant_id):
     response = yelp_api.business_query(id=restaurant_id)
-    return response #returns a Yelp BUSINESS dictionary with keys: name, id, location, etc.
+    return response  # returns a Yelp BUSINESS dictionary with keys: name, id, location, etc.
 
 def get_location_from_coordinates(long, lat) -> str:
     LOCATION = str(long) + "," + str(lat)
@@ -63,13 +73,87 @@ def get_location_from_coordinates(long, lat) -> str:
     location_name = jsonData['results'][0]['vicinity']
     return location_name
 
+    # get leftmost point, center both points by the left most point. find slope to determine how to find angle. find angle,
+    # rotate it, find box, rotate it back, translate it back.
 
-# pass in a list of tuples for locations. Default is SF and San Jose
-# category_filter is a list of category names
+def get_bounding_box(coords):
+
+    # width is Golden Ratio. L/1.62
+    long1 = coords[0][0]
+    long2 = coords[1][0]
+    left_most_point_index = np.array([long1,long2]).argmax()
+    right_most_point_index = left_most_point_index ^ 1 # XOR
+    translate_x = coords[left_most_point_index][0];
+    translate_y = coords[left_most_point_index][1];
+    recentered_outer_vector = np.array([coords[right_most_point_index][0]-translate_x, coords[right_most_point_index][1]-translate_y]);
+    if(recentered_outer_vector[1]==0 or recentered_outer_vector[0] == 0):
+        raise Exception (" error for now")
+    theta = np.arctan(recentered_outer_vector[0]/recentered_outer_vector[1])
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.matrix('{} {}; {} {}'.format(c, -s, s, c))
+    rotated_point = np.array(np.dot(R, recentered_outer_vector.transpose()))[0]
+    lower_left = np.array([-rotated_point[1]/1.62/2, 0])
+    lower_right = np.array([rotated_point[1]/1.62/2, 0])
+    upper_left = np.array([-rotated_point[1]/1.62/2, rotated_point[1]])
+    upper_right = np.array([rotated_point[1]/1.62/2, rotated_point[1]])
+    boundary_points = np.array([lower_left, lower_right, upper_left, upper_right]).transpose();
+    boundary_points = np.dot(np.linalg.inv(R), boundary_points); # rotate back to original angle.
+    translate_matrix = np.dot(np.array([[translate_x,0],[0,translate_y]]), np.ones([2,4]))
+    boundary_points = boundary_points + translate_matrix;
+    return boundary_points
+
+
+# pass in a list of tuples for locations. Default
+# category_filter is a list of category namesis SF and San Jose
+def get_results_from_locations(category_filter, coords=[(37.77493,-122.419415) , (37.3382, -121.8863)], num_of_results=40, limit = 20):
+    # parse location if two locations given
+
+    aliases = ''
+    for category in category_filter:
+        aliases += category_name_to_alias_dict[category] + ','
+    aliases = aliases[:-1]  # strip the last comma
+
+    if len(coords) == 2:
+        params = {
+            'category_filter': aliases,
+            'limit': limit,
+        };
+        # find bounding box
+
+        yelp_client.search_by_bounding_box(37.900000, -122.500000,37.788022,-122.399797, **params)
+
+    elif len(coords) == 1:
+        loc1 = coords[0];
+        long = loc1[0];
+        lat = loc1[1];
+        loc_coords = str(long) + "," + str(lat);
+        location = get_location_from_coordinates(long, lat);
+        print (loc_coords, location)
+
+    else:
+        raise Exception("Please provide no more than two location coordinate sets");
+    iterations = math.ceil(num_of_results / limit);
+
+    params = {
+        'category_filter': aliases,
+        'limit': limit,
+    };
+
+    client.search_by_coordinates(37.788022, -122.399797, **params)
+
+    responses = [];
+
+    for i in range(iterations):
+        time.sleep(.3)
+        response = yelp_api.search_query(category_filter=aliases, location=location, offset=limit*i, cll=loc_coords, limit=limit)
+        responses += response['businesses']  # list
+    return responses;
+
+'''
+# pass in a list of tuples for locations. Default
+# category_filter is a list of category namesis SF and San Jose
 def get_results_from_locations(category_filter, coords=[(37.77493,-122.419415) , (37.3382, -121.8863)], num_of_results=40 , limit = 20):
     # parse location if two locations given
-    loc_coords = "";
-    location = "";
     if len(coords) == 2:
         loc1 = coords[0]
         loc2 = coords[1]
@@ -97,13 +181,14 @@ def get_results_from_locations(category_filter, coords=[(37.77493,-122.419415) ,
     aliases = aliases[:-1]  # strip the last comma
 
     responses = [];
+
     for i in range(iterations):
-        time.sleep(.5)
+        time.sleep(.3)
         response = yelp_api.search_query(category_filter=aliases, location=location, offset=limit*i, cll=loc_coords, limit=limit)
         responses += response['businesses']  # list
     return responses;
 
-
+'''
 def get_yelp_data(list_businesses):
     list_yelp_data = [];
     for bus in list_businesses:
