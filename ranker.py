7078fd8610ee401d1972a8b1ef6473a530034dac
tfeng <tbhfeng@gmail.com>
2016-03-15 20:03:47 -0700

    added +.1 smoothing for probabilistic ranking

--- ranker.py
+++ ranker.py
@@ -17,7 +17,7 @@ def get_ranking_by_probabilistic_cosine(username, category_filter, coords=[(37.7
     for idx,yelp_data in enumerate(list_yelp_data):
         # yelp_data.cosine_sim = np.dot(user_weight_vec, yelp_data.restaurant_vector); # try dot product for more bias i think. add one smoothng can be done for dot product
         yelp_data.cosine_sim = 1 - spatial.distance.cosine(user_weight_vec, yelp_data.restaurant_vector);
-        list_cosine_sims[idx] = yelp_data.cosine_sim;
+        list_cosine_sims[idx] = yelp_data.cosine_sim + .1;
         # np.rad2deg(np.arccos(1- spatial.distance.cosine(user_weight_vec, yelp_data.restaurant_vector))) for degrees
     # sorts restaurants by probabilistics by their cosine similarity in an attempt to keep things interesting.
     list_yelp_data = np.random.choice(list_yelp_data, num_results, p=list_cosine_sims, replace=False).tolist()
